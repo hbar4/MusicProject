2bb42373c304b4dc849dc7e6fbdc598d56906233
CJordanGoodman <good.cjordan@gmail.com>
2018-02-13 15:05:40 -0800

    Update pulsating-circles.js

--- pulsating-circles.js
+++ pulsating-circles.js
@@ -1,4 +1,8 @@
 var y = 100;
+var pressed = false;
+var radius = 20;
+var increment = 5;
+var numCircles = 7;
 
 function setup() {
 	createCanvas(500, 500);
@@ -8,15 +12,34 @@ function setup() {
 function draw() {
 	clear();
 	background(0);
-	y = y - 1;
-	if (y < 0) {
-		y = height;
+	y = y + increment;
+	if (y < radius) {
+		y = radius;
+    increment *= -1;
 	}
+  if (y > height - radius) {
+    y = height - radius;
+    increment *= -1;
+  }
+  if (pressed) {
+    radius = 60;
+  } else {
+    radius = 20;
+  }
 	stroke(random(255));
 	fill("blue");
-	var mod = random(20);
-	ellipse(100, y, 20 + mod, 20+ mod);
-	ellipse(200, y, 20+ mod, 20+ mod);
-	ellipse(300, y, 20+ mod, 20+ mod);
-	ellipse(400, y, 20+ mod, 20+ mod);
+	var mod = random(radius);
+  var circleY = y;
+  for (var i = 0; i < numCircles; i++) {
+    ellipse((i + 1)*width/(numCircles + 1), circleY, radius + mod, radius + mod);
+  	circleY = height - circleY;
+  }
+}
+
+function keyPressed() {
+  pressed = true;
+}
+
+function keyReleased() {
+	pressed = false; 
 }
